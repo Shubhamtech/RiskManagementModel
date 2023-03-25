4b2a6e2360125d8ca9d1e2f38f0c2cf03af18e62
Manisa Basak <basak.manisha94@gmail.com>
2023-03-25 16:48:20 +0530

    Done CSS

--- src/App.js
+++ src/App.js
@@ -1,61 +1,66 @@
-
-import './App.css';
+import "./App.css";
 import Heading from "./components/Heading";
-import Papa from 'papaparse';
-import { useState } from 'react';
+import Papa from "papaparse";
+import { useState } from "react";
 import PDFFile from "./components/PDFFile";
-import { PDFDownloadLink } from '@react-pdf/renderer';
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { Button } from "@mui/material";
+import { Box, Container } from "@mui/system";
 function App() {
-  const [data,setData]=useState([]);
-  const [columnArray,setColumn]=useState([]);
-  const [values,setValues]=useState([]);
+  const [data, setData] = useState([]);
+  const [columnArray, setColumn] = useState([]);
+  const [values, setValues] = useState([]);
 
-  const handleFile=(event)=>{
-   Papa.parse(event.target.files[0],{
-    header:true,
-    skipEmptyLines:true,
-    complete: function(result){
-      const columnArray=[];
-      const valueArray=[];
-      result.data.map((d)=>{
-        columnArray.push(Object.keys(d));
-        valueArray.push(Object.values(d));
-
-      });
-      setData(result.data);
-      setColumn(columnArray[0]);
-      setValues(valueArray);
-    }
-   })
-  }
+  const handleFile = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (result) {
+        const columnArray = [];
+        const valueArray = [];
+        result.data.map((d) => {
+          columnArray.push(Object.keys(d));
+          valueArray.push(Object.values(d));
+        });
+        setData(result.data);
+        setColumn(columnArray[0]);
+        setValues(valueArray);
+      },
+    });
+  };
   return (
-    <div className="App">
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          m: 2,
+          mt: 20,
+          mb: 10,
+        }}>
         <Heading />
-       
+      </Box>
+      <Box sx={{ border: 1, p: 4, alignItems: "center" }}>
         <input
           type="file"
-          name='file'
-          accept='.csv'
+          name="file"
+          accept=".csv"
           onChange={handleFile}
-          style={{display:"block",margin:"10px auto"}}
-        >
-        </input>
-        <p>
-         this is a model for risk  checking
-        </p>
-        <br/>
-        <PDFFile />
-       <PDFDownloadLink document={<PDFFile />} fileName="FORM">
-        {({loading})=> loading? (
-         <button>Loading document..</button> 
-         ) : (
-         <button className="btn">Download</button>
-         )
-         }
-       </PDFDownloadLink>
-       
-    </div>
+          style={{ display: "block", margin: "10px auto" }}></input>
+        <br />
+        <PDFDownloadLink document={<PDFFile />} fileName="FORM">
+          {({ loading }) =>
+            loading ? (
+              <button>Loading document..</button>
+            ) : (
+              <Box sx={{ alignItems: "center", mx: "auto",pl:24
+               }}>
+                <Button variant="contained">Download</Button>
+              </Box>
+            )
+          }
+        </PDFDownloadLink>
+      </Box>
+    </Container>
   );
-};
+}
 
 export default App;
